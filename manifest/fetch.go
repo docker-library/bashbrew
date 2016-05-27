0b0d4937247051c1c554e5c2d60a8ff810d98c2d
Tianon Gravi <admwiggin@gmail.com>
2016-05-27 11:57:01 -0700

    Change the order manifest.Fetch parses so we can have a good fallback error message

--- manifest/fetch.go
+++ manifest/fetch.go
@@ -1,17 +1,35 @@
 package manifest
 
 import (
+	"fmt"
 	"os"
 	"net/http"
+	"net/url"
 	"path"
 	"path/filepath"
 )
 
-// "dir" is the default "library directory"; returns the parsed version of (in order) the file "repo", the file "dir/repo", or the remote file fetched from the URL "repo"
+// "dir" is the default "library directory"
+// returns the parsed version of (in order):
+//   if "repo" is a URL, the remote contents of that URL
+//   the file "repo"
+//   the file "dir/repo"
 func Fetch(dir, repo string) (string, *Manifest2822, error) {
 	repoName := path.Base(repo)
 
-	// try file paths first
+	_, err := url.Parse(repo)
+	if err == nil {
+		// must be remote URL!
+		resp, err := http.Get(repo)
+		if err != nil {
+			return repoName, nil, err
+		}
+		defer resp.Body.Close()
+		man, err := Parse(resp.Body)
+		return repoName, man, err
+	}
+
+	// try file paths
 	for _, fileName := range []string{
 		repo,
 		filepath.Join(dir, repo),
@@ -27,14 +45,5 @@ func Fetch(dir, repo string) (string, *Manifest2822, error) {
 		}
 	}
 
-	// must be remote URL
-	resp, err := http.Get(repo)
-	if err != nil {
-		return repoName, nil, err
-	}
-	defer resp.Body.Close()
-	man, err := Parse(resp.Body)
-	return repoName, man, err
-
-	//return repoName, nil, fmt.Errorf("unable to find a manifest named %q (in %q or as a remote URL)", repo, dir)
+	return repoName, nil, fmt.Errorf("unable to find a manifest named %q (in %q or as a remote URL)", repo, dir)
 }
