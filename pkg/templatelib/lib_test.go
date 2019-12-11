8e25067487f7953b1a1604050814390d3f1c45cd
Tianon Gravi <admwiggin@gmail.com>
2019-12-10 16:20:17 -0800

    Update Go versions to 1.12 and 1.13
    See https://golang.org/doc/go1.12#text/template, especially:
    
    > If a user-defined function called by a template panics, the panic is now caught and returned as an error by the `Execute` or `ExecuteTemplate` method.

--- pkg/templatelib/lib_test.go
+++ pkg/templatelib/lib_test.go
@@ -1,6 +1,7 @@
 package templatelib_test
 
 import (
+	"strings"
 	"testing"
 	"text/template"
 	"unsafe"
@@ -11,35 +12,31 @@ import (
 func TestTernaryPanic(t *testing.T) {
 	// one of the only places template.IsTrue will return "false" for the "ok" value is an UnsafePointer (hence this test)
 
-	defer func() {
-		if r := recover(); r == nil {
-			t.Errorf("Expected panic, executed successfully instead")
-		} else if errText, ok := r.(string); !ok || errText != `template.IsTrue(<nil>) says things are NOT OK` {
-			t.Errorf("Unexpected panic: %v", errText)
-		}
-	}()
-
 	tmpl, err := template.New("unsafe-pointer").Funcs(templatelib.FuncMap).Parse(`{{ ternary "true" "false" . }}`)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
 
 	err = tmpl.Execute(nil, unsafe.Pointer(uintptr(0)))
-	if err != nil {
-		t.Errorf("Expected panic, got error instead: %v", err)
+	if err == nil {
+		t.Errorf("Expected error, executed successfully instead")
+	}
+	if !strings.HasSuffix(err.Error(), `template.IsTrue(<nil>) says things are NOT OK`) {
+		t.Errorf("Expected specific error, got: %v", err)
 	}
 }
 
 func TestJoinPanic(t *testing.T) {
-	defer func() {
-		if r := recover(); r == nil {
-			t.Errorf("Expected panic, executed successfully instead")
-		} else if errText, ok := r.(string); !ok || errText != `"join" requires at least one argument` {
-			t.Errorf("Unexpected panic: %v", r)
-		}
-	}()
-
 	tmpl, err := template.New("join-no-arg").Funcs(templatelib.FuncMap).Parse(`{{ join }}`)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
 
 	err = tmpl.Execute(nil, nil)
-	if err != nil {
-		t.Errorf("Expected panic, got error instead: %v", err)
+	if err == nil {
+		t.Errorf("Expected error, executed successfully instead")
+	}
+	if !strings.HasSuffix(err.Error(), `"join" requires at least one argument`) {
+		t.Errorf("Expected specific error, got: %v", err)
 	}
 }
