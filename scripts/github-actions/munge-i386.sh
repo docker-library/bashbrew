3af45bd40255b80134fc1806b6eac58b45491f44
Tianon Gravi <admwiggin@gmail.com>
2020-04-24 15:48:17 -0700

    Add initial scripts for image maintainers to use to auto-generate appropriate GitHub Actions

---
+++ scripts/github-actions/munge-i386.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+jq --arg dpkgSmokeTest '[ "$(dpkg --print-architecture)" = "amd64" ]' '
+	.matrix.include += [
+		.matrix.include[]
+		| select(.name | test(" (.+)") | not) # ignore any existing munged builds
+		| select(.os | startswith("windows-") | not)
+		| .name += " (i386)"
+		| .runs.pull = ([
+			"# pull i386 variants of base images for multi-architecture testing",
+			$dpkgSmokeTest,
+			(
+				.meta.froms[]
+				| ("i386/" + . | @sh) as $i386
+				| (
+					"docker pull " + $i386,
+					"docker tag " + $i386 + " " + @sh
+				)
+			)
+		] | join("\n"))
+	]
+' "$@"
