8c7ed364706dcab8d1810dde28d5c765c1f3938a
Tianon Gravi <admwiggin@gmail.com>
2021-02-01 11:00:04 -0800

    Add explicit Manifest2822Maintainer struct
    This allows us to more easily/reliably generate lists of users to ping on PRs (with the goal to ping them automatically, if one of them is not the PR author).
    
    This is technically a breaking change, but I consider myself probably the most prolific user of bashbrew format strings (which is where this breakage would exhibit itself -- `bashbrew cat` should be unaffected), and I have exactly one script broken by it.  That script was doing some processing to generate this exact data, so IMO this is a win. 😁
    
        bashbrew cat --format='- `{{ $.RepoName }}`:{{ range .Manifest.Global.Maintainers }} @{{ .Handle }}{{ end }}' ...

--- manifest/parse_test.go
+++ manifest/parse_test.go
@@ -13,8 +13,24 @@ func TestParseError(t *testing.T) {
 	man, err := manifest.Parse(strings.NewReader(invalidManifest))
 	if err == nil {
 		t.Errorf("Expected error, got valid manifest instead:\n%s", man)
+		return
 	}
 	if !strings.HasPrefix(err.Error(), "Bad line:") {
 		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+}
+
+func TestInvalidMaintainer(t *testing.T) {
+	testManifest := `Maintainers: Valid Name (@valid-handle), Valid Name <valid-email> (@valid-handle), Invalid Maintainer (invalid-handle)`
+
+	man, err := manifest.Parse(strings.NewReader(testManifest))
+	if err == nil {
+		t.Errorf("Expected error, got valid manifest instead:\n%s", man)
+		return
+	}
+	if !strings.HasPrefix(err.Error(), "invalid Maintainers:") {
+		t.Errorf("Unexpected error: %v", err)
+		return
 	}
 }
