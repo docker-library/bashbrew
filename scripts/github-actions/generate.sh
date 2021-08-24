fd67e46740a629b8d4cd89724bee34ec2bb20f6d
Tianon Gravi <admwiggin@gmail.com>
2021-08-24 14:38:14 -0700

    Swap order to be descending

--- scripts/github-actions/generate.sh
+++ scripts/github-actions/generate.sh
@@ -58,10 +58,10 @@ for tag in $tags; do
 			{
 				name: .name,
 				os: (
-					if (.constraints | contains(["windowsservercore-1809"])) or (.constraints | contains(["nanoserver-1809"])) then
-						"windows-2019"
-					elif (.constraints | contains(["windowsservercore-ltsc2022"])) or (.constraints | contains(["nanoserver-ltsc2022"])) then
+					if (.constraints | contains(["windowsservercore-ltsc2022"])) or (.constraints | contains(["nanoserver-ltsc2022"])) then
 						"windows-2022"
+					elif (.constraints | contains(["windowsservercore-1809"])) or (.constraints | contains(["nanoserver-1809"])) then
+						"windows-2019"
 					elif .constraints | contains(["windowsservercore-ltsc2016"]) then
 						"windows-2016"
 					elif .constraints == [] or .constraints == ["!aufs"] then
